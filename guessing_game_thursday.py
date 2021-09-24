2fda94d4f06adb7c554a9f82ad688391fb62d313
Raluca Baceanu <raluca.baceanu@gmail.com>
2021-09-24 18:24:52 +0300

    day 4: fixed small bug

--- guessing_game_thursday.py
+++ guessing_game_thursday.py
@@ -13,10 +13,10 @@ user_input = input("What number am I thinking of? From 1 to 9 only:  ")
 
 
 def guessing_game(typed_input):
-    guess = random.randint(1, 9)
+    guess = random.randint(1, 10)
     typed_input = user_input
-    if len(typed_input) >= 2:
-        print('Please fill in a number from 0 to 9!')
+    if len(typed_input) > 2:
+        print('Please fill in a number from 0 to 10!')
     else:
         try:
             typed_input = int(user_input)
@@ -24,12 +24,16 @@ def guessing_game(typed_input):
             print('Hey, please make sure you type in a number, not letters or something else!')
         else:
             if type(typed_input == int):
-                if guess < typed_input:
-                    print(f'It should have been a little lower, it was {str(guess)}.')
-                elif guess > typed_input:
-                    print(f'It should have been higher...uf, it was {str(guess)}.')
+                if typed_input <= 10:
+                    if guess < typed_input:
+                        print(f'It should have been a little lower, it was {str(guess)}.')
+                    elif guess > typed_input:
+                        print(f'It should have been higher...uf, it was {str(guess)}.')
+                    else:
+                        print(f'Wow, you guessed it was {str(guess)} unbelievable!')
                 else:
-                    print(f'Wow, you guessed it was {str(guess)} unbelievable!')
+                    if typed_input > 10:
+                        print('The number is greater than 10, please fill in another!')
 
 
 if __name__ == '__main__':
